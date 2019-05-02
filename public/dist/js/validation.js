89af9667e33400573cc1bfc0d920c5370462b6ba
hamsahmedansari <hams.ahmed.ansari95@gmail.com>
2019-05-02 16:49:02 +0500

    Update: :construction: getting data from api to front-end  2 is Modified :  1 : public/dist/js/validation.js  2 : templates/views/index.hbs

--- public/dist/js/validation.js
+++ public/dist/js/validation.js
@@ -1,11 +1,31 @@
 $(document).ready(() => {
   const searchInput = $(".search-input");
   const searchBtn = $(".search-btn");
+  const searchForm = $(".search-form");
 
+  searchForm.on("submit", e => {
+    e.preventDefault();
+    searchByCity();
+  });
   searchBtn.on("click", e => {
+    searchByCity();
+  });
+
+  function searchByCity() {
     if (searchInput.val() === "") {
       return false;
     }
-    const searchValue = searchInput.val();
-  });
+    const searchValue = String(searchInput.val())
+      .trim()
+      .toLowerCase();
+    searchInput.val("");
+    fetch(`${location.href}api/city?name=${searchValue}`)
+      .then(res => res.json())
+      .then(res => {
+        console.log(res);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
 });
