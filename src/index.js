555d51228a5b6ebcea2227f21a29ec3d02cf4830
hamsahmedansari <hams.ahmed.ansari95@gmail.com>
2019-05-02 15:00:24 +0500

    Update: :construction: connecting api on search  4 is Modified :  1 : package-lock.json  2 : package.json  3 : public/index.html  4 : src/index.js  1 is Created :  1 : public/dist/js/validation.js

--- src/index.js
+++ src/index.js
@@ -3,10 +3,15 @@ const app = express();
 
 const path = require("./util/path");
 
+// view engine
+// app.set("view engine", "hbs");
+
 // routes
 app.use(express.static(path.public)); //Static
 
-
+// app.get("/", (req, res) => {
+//   res.render("index");
+// });
 // server
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`App is running on Port ${port}`));
